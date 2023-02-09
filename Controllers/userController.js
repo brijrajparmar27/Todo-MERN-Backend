71bb08243686220542a6f3237f3ef1fc4645a3fb
brijraj <brijrajparmar27@gmail.com>
2023-02-09 11:36:52 +0530

    fixed fetch realtime projects and signup project attribute missing

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -2,41 +2,69 @@ const user = require("../Models/userModel");
 const jwt = require("jsonwebtoken");
 
 const createToken = ({ _id }) => {
-    return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: '3d' })
-}
+  return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: "3d" });
+};
 
 const login = (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    user.login(email, password).then(data => {
-        let token = createToken(data);
-        res.json({ _id: data._id, jwt: token, email: data.email, projects: data.projects }).status(200);
-    }).catch(err => {
-        res.json({ message: err.message }).status(400);
+  user
+    .login(email, password)
+    .then((data) => {
+      let token = createToken(data);
+      res
+        .json({
+          _id: data._id,
+          jwt: token,
+          email: data.email,
+          projects: data.projects,
+        })
+        .status(200);
     })
-}
+    .catch((err) => {
+      res.json({ message: err.message }).status(400);
+    });
+};
 
 const signup = (req, res) => {
-    const { email, password } = req.body;
-    user.signup(email, password).then((data) => {
-        let token = createToken(data);
-        res.json({ _id: data._id, jwt: token, email: data.email }).status(200);
-    }).catch(err => {
-        res.json({ message: err.message }).status(400);
+  const { email, password } = req.body;
+  user
+    .signup(email, password)
+    .then((data) => {
+      let token = createToken(data);
+      res
+        .json({
+          _id: data._id,
+          jwt: token,
+          email: data.email,
+          projects: data.projects,
+        })
+        .status(200);
     })
-}
+    .catch((err) => {
+      res.json({ message: err.message }).status(400);
+    });
+};
 
 const AddProject = (req, res) => {
-    const { projects, _id } = req.body;
-    console.log(projects, _id);
-    user.findOneAndUpdate({ _id }, { projects }).then(data => {
-        console.log(data);
-        res.json(data).status(200);
-    })
-}
+  const { projects, _id } = req.body;
+  console.log(projects, _id);
+  user
+    .findOneAndUpdate(
+      { _id },
+      { projects },
+      {
+        new: true,
+      }
+    )
+    .then((data) => {
+      console.log(data, " updated projects");
+      res.json(data).status(200);
+    });
+};
 
 module.exports = {
-    login,
-    signup,
-    AddProject
-}
+  login,
+  signup,
+  AddProject,
+};
